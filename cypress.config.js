baa5c4ea0dd1775470f482ebdefed1b305173383
Suresh Sathyanarayan <sureshsathyanarayan@sureshs-mbp.lan>
2025-10-07 08:30:16 -0500

    small changes

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     experimentalRunAllSpecs: true,
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     pageLoadTimeout: 60000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
